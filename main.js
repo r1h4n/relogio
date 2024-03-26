4479eb7f6e89c2df6834755fe306a39d32f37435
r1h4n <111518762+r1h4n@users.noreply.github.com>
2024-03-26 12:35:33 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
-const botoes =document.querySelectorAll(".botao");
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
-for(let i = 0;i < botoes.length; i++){
-    botoes[i].onclick = function (){
-        for(let j = 0; j < botoes.length;j++){
+//laço de repetição
+
+for(let i = 0; i < botoes.length; i++){
+    botoes[i].onclick = function(){
+        for (let j = 0; j < botoes.length; j++){
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
-        botoes[i].classList.add("ativo"); 
-   }
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
+}
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-03-26T12:10:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+
+function atualizarTempo(tempoObjetivo1){
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo1 - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor (segundos / 60);
+    let horas = Math.floor (minutos / 60);
+    let dias = Math.floor (horas / 24);
 }
